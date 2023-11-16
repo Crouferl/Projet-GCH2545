079b8295dcaa1ff77a25a08dcd3c9e24d3b4baf7
Crouferl <leo.croufer@polymtl.ca>
2023-11-16 16:05:48 -0500

    Ajout des paramètres

--- patinoire_fct.py
+++ patinoire_fct.py
@@ -7,24 +7,25 @@
     #Input : erreur voulue
     #Out : Nombre de points en x et en y
 
-#Interpoler température lagrange
+#Interpoler température lagrange (RAPH)
     #Input : temps, température
     #Out : Coefficients du polyôme
 
 #Résidus : Calcul le résidu du système d'équation
     #Input : valeur actuelle (T), valeur intiale (Ti), prm, pas de temps
     #Out : résidus
+    #Calculs à la main nécessaire
 
 #Jacobienne : Calcul jacobienne numérique
     #Input : Valeur actuelle (T), prm, pas de temps
     #Out : matrice jacobienne
+    #Calculs à la main
 
-
-# Euler implicite
+# Euler implicite (RAPH)
     #Input : conditions initiales, pas de temps, temps final, tolérance, prm
     #Out : Température, temps
 
-#mesh : discrétiser l'espace à étudier
+#mesh : discrétiser l'espace à étudier (RAPH)
     #Input : nx,ny,x_min, x_max, z_min, z_max
     #Out : matrices des positions x, matrices des positions y
 
