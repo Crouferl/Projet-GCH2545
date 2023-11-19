1b30c156739239a00e5b9d6b0d6ada667284629e
Crouferl <leo.croufer@polymtl.ca>
2023-11-19 17:48:58 -0500

    Ajouter prm

--- patinoire_fct.py
+++ patinoire_fct.py
@@ -11,30 +11,16 @@
     #Input : temps, température
     #Out : Coefficients du polyôme
 
-#Résidus : Calcul le résidu du système d'équation
-    #Input : valeur actuelle (T), valeur intiale (Ti), prm, pas de temps
-    #Out : résidus
-    #Calculs à la main nécessaire
-
-#Jacobienne : Calcul jacobienne numérique
-    #Input : Valeur actuelle (T), prm, pas de temps
-    #Out : matrice jacobienne
-    #Calculs à la main
-
-# Euler implicite (RAPH)
-    #Input : conditions initiales, pas de temps, temps final, tolérance, prm
-    #Out : Température, temps
-
 #mesh : discrétiser l'espace à étudier (RAPH)
     #Input : nx,ny,x_min, x_max, z_min, z_max
     #Out : matrices des positions x, matrices des positions y
 
 #mdf : remplir les matrices A et B
     #Input : Bornes x, bornes y, nx, nz, prm
-    #Out :Matrice A et matrice B
+    #Out : matrices de la température à chaque position pour chaque pas de temps
     #Note : calculs euler implicite à faire pour remplir matrice 
 
-# Plot (Optionnel) :
+# Plot (Optionnel) : 
     #Input : titre, noms des axes 
     #Out : oject figure
 
