4c4536a6b14e7e0b6f332a4140b1a51275e3d8ae
Crouferl <111090134+Crouferl@users.noreply.github.com>
2023-11-22 10:07:48 -0500

    Modification de la mdf et ajout de Ti dans prm

--- patinoire_mdf_transitoire.py
+++ patinoire_mdf_transitoire.py
@@ -9,7 +9,35 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def mdf(X_limit,Z_limit,nx,nz,prm,dt,tf,ci) :
+
+def temperature_liquide(t) :
+    
+    """Fonction qui retourne un array de la température du liquide en fonction du temps
+    Entrées:
+        - t = array de point de temps de 0 à tf
+
+    Sortie (dans l'ordre énuméré ci-bas):
+        - temp_liquide : array tf/dt x 2 [Temps, température liquide]
+        
+    """
+    n_step = tf//dt
+    temp_liquide = np.zeros(n_step,2)
+    temp_instant = -1
+    
+    for i,t in enumerate(t):
+        
+        temp_liquide[i,0] = t
+        temp_liquide[i,1] = temp_instant
+        
+        if temp_instant >-15 :
+            temp_instant -=1
+        else :
+            temp_instant = -15
+        
+    return temp_liquide
+
+
+def mdf(X_limit,Z_limit,nx,nz,prm,dt,tf) :
     
     """Fonction qui résoud le problème de différence fini et calcule la position pour chaque point pour chaque pas de temps
     Entrées:
@@ -20,26 +48,28 @@ def mdf(X_limit,Z_limit,nx,nz,prm,dt,tf,ci) :
         - prm : objet contenant tous les paramètres physiques du problème
         - dt : pas de temps 
         - tf : temps final
-        - ci : Matrice contenant la température initiale de chaque point [# numéro de point, température]
 
     Sortie (dans l'ordre énuméré ci-bas):
         - points_temps : Array contenants les points de temps où la température a été calculée
         - temperature_store : Array contenant la température pour chaque point à chaque point de temps 
-            - [[# de point, température Temps 0],[# de points, Températuer Temp 1].....]
+            - [[température Temps 0],[Températeur Temp 1].....]
         
     """
     X_pos, Z_pos = mesh(X_limit,Z_limit,nx,nz) #Créer les matrices des positions pour les deux dimensions
     points = numeroter_mesh(X_pos,Z_pos) #Créer la matrices avec les positions et les adresses de chaque points
-    points_temps = np.linspace(0,tf,dt,dtype="float") # Créer la matrice des points de temps où le problème sera évalué
     n_points = nx*nz #Nombre de points dans la discrétisation
 
     #Pas de discrétisation du domaine physique
     dx = abs((X[1]-X[0])/(nx-1))
     dz = abs((Z[1]-Z[0])/(nz-1))
+    
+    ci = np.ones(n_points)*prm.Ti
 
 
     temperature_store = np.zeros(len(points_temps)) #Matrice pour enregister les résulats finals
     temp = np.zeros(n_points) #Matrice temporaire pour stocker le résultat de chaque itération
+    
+    t_L = temperature_liquide(points_temps)
 
     for i,t in enumerate(points_temps) : #Boucle pour itérer sur chaque point de temps
 
@@ -53,24 +83,34 @@ def mdf(X_limit,Z_limit,nx,nz,prm,dt,tf,ci) :
             
 
             if Z == Z_limit[0] : #Vérifier si le point est sur la limite inférieure
-                #Remplir A et B avec condition limite dessous
-               
-            elif Z == Z_limit[1] : #Vérifier si le point est sur la limite supérieure
-                #Remplir A et B avec condition limite dessus
-                
+                   
+                A[i,i+2] = -1
+                A[i,i+1] = 4
+                A[i,i] = (2*dz*prm.h_l)/(prm.k_b) - 3
+                B[i] = (2*dz*prm.h_l*t_L[i])/(prm.k_b) 
 
+            elif Z == Z_limit[1] : #Vérifier si le point est sur la limite supérieure
+                A[i,i] = (3)/(2*dz) + (prm.h_air)/(prm.k_g)
+                A[i,i-1] = (-4)/(2*dz)
+                A[i,i-2] = 1/(2*dz)
+                B[i] = (prm.h_air*interpolation_Tair(t))/prm.k_g  #!!!!! FONCTION D'INTERPOLATION DE TAIR À VALIDER
+            
             else : #Remplir le coeur de la matrice
                 
                 if Z < prm.zb : #Vérifier si le point est dans la glace ou dans le béton
                     alpha = prm.alpha_b #
                 else : 
                     alpha = prm.alpha_g
-                #Remplir A et B avec coeur du problème
+                    
+                A[i,i+1] = -(alpha*dt)/(dz**2)
+                A[i,i] = 1 +(2*alpha*dt)/(dz**2)
+                A[i,i-1] = -(alpha*dt)/(dz**2)
+                B[i] = ci[i]
+                
+        temp = np.linalg.solve(A,B) 
 
-        temp = np.linalg.solve(A,B) #Résoudre le problème de différence fini
+        ci = temp.copy() 
 
-        ci = temp.copy() #Mettre à jour les conditions initiales
-
-        temperature_store[i] = temp #Enregistrer la température de chaque point
+        temperature_store[i] = temp 
 
     return points_temps, temperature_store 
