c29797f71ed6f9e47d84f2dd3345e7a47b611ec9
Crouferl <111090134+Crouferl@users.noreply.github.com>
2023-11-22 10:10:51 -0500

    Modifier MDF patinoire_fct

---
+++ Test_bench_MDF.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov 22 10:10:04 2023
+
+@author: crouf
+"""
+
+# Importation des modules
+import numpy as np
+import matplotlib.pyplot as plt
+import pytest
+try:
+    from patinoire_fct import *
+except:
+    pass
+
+#---------------------------------------------------------------------------
+
+
+#Définir paramètres du problème
+
+class parametre():
+    zb = 0.05 #[m]
+    zg = 0.10 #[m]
+    alpha_b = 0.54 #[ms−1]
+    alpha_g = 1.203 #[ms−1]
+    k_b = 0.6 #[Wm−1 K−1]
+    k_g = 2.1 #[Wm−1 K−1]
+    h_l = 300 #[Wm−2 K−1]
+    h_air = 5 #[Wm−2 K−1]
+    Ti = -1 #[°C]
+
+prm = parametre()
+#-----------------------------------------------------------------------------
+
